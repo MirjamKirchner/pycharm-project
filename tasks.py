dd1cb7daf2b9e683a692b38a17b90ce92c05c181
Mirjam Kirchner <riedel.ks@gmail.com>
2023-06-01 07:40:45 +0200

    [INCOMPLETE]Updated .pre-commit-config.yaml and task.py

--- tasks.py
+++ tasks.py
@@ -1,21 +1,9 @@
-# -1 define pre-requisites: poetry, python3
-# 0. set up virtual environment as venv
-# 1. install requirements and install into venv
-# Enable pre-commit hooks
-# Setup file watchers
-# In pycharm, set Tools --> Python integrated tools --> Doc String format --> NumPy
-from warnings import warn
 from invoke import task
-from invoke.exceptions import UnexpectedExit
-from typing import Final
+from invoke.context import Context
 
-POETRY_BOOLS: Final = {
-    True: "true",
-    False: "false"
-}
 
 @task
-def setup(context):  # tasks are not allowed to have underscores
+def setup(context: Context):  # tasks are not allowed to have underscores
     """
     Prints 'Hello, world!'
 
@@ -28,52 +16,44 @@ def setup(context):  # tasks are not allowed to have underscores
     -------
     None
     """
-    _setup_pvenv(context)
+    # TODO check whether project is already set up and abort if so
+    _initialize_poetry(context)
     _install_dependencies(context)
+    _activate_venv(context)
+    _activate_precommit_hooks(context)
 
 
-def _setup_venv(
-        context,
-        python_version: str = "system",
-        option: str = True
-):
-    cmd_poetry_version = "poetry --version"
-    try:
-        print("-----------------------------------------------------------------------------------------------------\n"
-              "Tools:\n"
-              "Dependency management and packaging tool:")
-        context.run(cmd_poetry_version)
-        print("-----------------------------------------------------------------------------------------------------\n")
-    except UnexpectedExit:
-        raise(
-            "Failed to execute command '{command}'.\n"
-            "This indicates that the required dependency management and packaging tool 'Poetry' is not installed.\n"
-            "Check out 'https://python-poetry.org/' for installation instructions.".format(
-                command=cmd_poetry_version
-            )
-        )
-    # Update pyproject.toml using information from git
-    # Add black, pylint, ...
-    # Enable options https://python-poetry.org/docs/cli/?ref=serpapi.com#install
-    cmd_setup_venv = "poetry install" + option
-    print("I will setup the virtual environment using poetry!")
+def _initialize_poetry(context: Context):
+    context.run("poetry init")
+    context.run("git add pyproject.toml")
 
 
-def _install_dependencies(context):
-    # black -- formatting
-    # pytest -- testing
-    # pre-commit -- pre-commit hooks
-    # mypy -- type hints
-    # pylint(, flake8 ?) -- code linter
-    print("I will install all project requirements!")
+def _install_dependencies(context: Context):
+    dependencies = (
+        "black --group coding",  # Code formatting (PEP-8)
+        "isort --group coding",  # Code formatting (order imports)
+        "pylint --group coding",  # Code style
+        "mypy --group coding",  # Type hints
+        "pytest --group testing",  # Tests
+        "pre-commit --group cicd",  # Pre-commit hooks
+        "invoke --group cicd",  # CLI-invokable tasks
+    )
+    for dependency in dependencies:
+        context.run("poetry add " + dependency)
 
 
-def _activate_precommit_hooks():
-    print("I will activate pre-commit hooks!")
+def _activate_precommit_hooks(context: Context):
+    context.run("pre-commit install")
+    context.run("pre-commit autoupdate")
+    context.run("git add .pre-commit-config.yaml")  # This has no effect
 
 
-def _activate_file_watchers():
+def _activate_file_watchers(context: Context):
     print("I will activate file watchers")
+
+
+def _activate_venv(context: Context):
+    context.run("poetry shell")
 
 
 if __name__ == "__main__":
